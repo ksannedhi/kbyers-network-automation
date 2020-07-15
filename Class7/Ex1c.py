f7d08b00f97cc8a414d14e7a1b8147ce2d6911a9
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 14:39:45 +0530

    Update Ex1c.py

--- Class7/Ex1c.py
+++ Class7/Ex1c.py
@@ -1,3 +1,6 @@
+'''Print out the root element tag name (this tag should have a value of "zones-information").
+Print the number of child elements of the root element (you can retrieve this using the len() function).'''
+
 from lxml import etree
 
 xml_file = open("Ex1_show_security_zones.xml")
@@ -8,4 +11,3 @@ my_xml = etree.fromstring(xml_data)
 print(my_xml.tag)
 print(len(my_xml.getchildren()))
 print(len(my_xml)) #Alternative method
-
