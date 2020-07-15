630eed8f5638f824bb9be7c016402e3c8ec191e5
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 13:23:23 +0530

    Update Ex7_show_int_status.py

--- Class4/Ex7_show_int_status.py
+++ Class4/Ex7_show_int_status.py
@@ -1,3 +1,33 @@
+'''Using your TextFSM template and the 'show interface status' data from exercise2, create a Python program that uses TextFSM to parse this data.
+In this Python program, read the show interface status data from a file and process it using the TextFSM template.
+From this parsed-output, create a list of dictionaries. The program output should look as follows:
+$ python ex7_show_int_status.py 
+
+[{'DUPLEX': 'auto',
+  'PORT_NAME': 'Gi0/1/0',
+  'PORT_TYPE': '10/100/1000BaseTX',
+  'SPEED': 'auto',
+  'STATUS': 'notconnect',
+  'VLAN': '1'},
+ {'DUPLEX': 'auto',
+  'PORT_NAME': 'Gi0/1/1',
+  'PORT_TYPE': '10/100/1000BaseTX',
+  'SPEED': 'auto',
+  'STATUS': 'notconnect',
+  'VLAN': '1'},
+ {'DUPLEX': 'auto',
+  'PORT_NAME': 'Gi0/1/2',
+  'PORT_TYPE': '10/100/1000BaseTX',
+  'SPEED': 'auto',
+  'STATUS': 'notconnect',
+  'VLAN': '1'},
+ {'DUPLEX': 'auto',
+  'PORT_NAME': 'Gi0/1/3',
+  'PORT_TYPE': '10/100/1000BaseTX',
+  'SPEED': 'auto',
+  'STATUS': 'notconnect',
+  'VLAN': '1'}]'''
+
 import textfsm
 from pprint import pprint
 
@@ -30,4 +60,3 @@ for intf_entry in sh_int_status_op:
     sh_int_status_list.append(intf_dict)
 
 pprint(sh_int_status_list)
-
