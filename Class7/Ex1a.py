540e74a42950d97d4df6b67c5c1bd3c05e39e87c
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 14:37:18 +0530

    Update Ex1a.py

--- Class7/Ex1a.py
+++ Class7/Ex1a.py
@@ -1,3 +1,8 @@
+'''Using the Ex1_show_security_zones.xml file, read the file contents and parse the file using etree.fromstring().
+Print out the newly created XML variable and also print out the variable's type. Your output should look similar to the following:
+<Element zones-information at 0x7f3271194b48>
+<class 'lxml.etree._Element'>'''
+
 from lxml import etree
 
 xml_file = open("Ex1_show_security_zones.xml")
