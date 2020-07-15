063d9d1589017a1f37ab0f55c3d07cd305a4b993
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 14:41:33 +0530

    Update Ex1e.py

--- Class7/Ex1e.py
+++ Class7/Ex1e.py
@@ -1,3 +1,6 @@
+'''Create a variable named "trust_zone". Assign this variable to be the first "zones-security" element in the XML tree.
+Access this newly created variable and print out the text of the "zones-security-zonename" child.'''
+
 from lxml import etree
 
 xml_file = open("Ex1_show_security_zones.xml")
