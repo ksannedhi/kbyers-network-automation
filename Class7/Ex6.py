380d52d78cbcb3736ca2280ddb307820a49da20a
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 15:04:21 +0530

    Update Ex6.py

--- Class7/Ex6.py
+++ Class7/Ex6.py
@@ -1,3 +1,7 @@
+'''Create an nxapi_plumbing "Device" object for nxos1. The api_format should be "jsonrpc" and the transport should be "https" (port 8443).
+Use getpass() to capture the device's password. Send the "show interface Ethernet1/1" command to the device, parse the output, and print out the following information:
+Interface: Ethernet1/1; State: up; MTU: 1500'''
+
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from getpass import getpass
@@ -12,4 +16,3 @@ output = device.show("show interface Ethernet1/1")
 print(f"Interface: {output['TABLE_interface']['ROW_interface']['interface']}; "
       f"State: {output['TABLE_interface']['ROW_interface']['admin_state']}; "
       f"MTU: {output['TABLE_interface']['ROW_interface']['eth_mtu']}")
-
