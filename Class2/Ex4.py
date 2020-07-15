55064cf302be961303a16a30cfc89f8219f9ee75
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 06:11:00 +0530

    Update Ex4.py

--- Class2/Ex4.py
+++ Class2/Ex4.py
@@ -1,3 +1,13 @@
+'''Use Netmiko and the send_config_set() method to configure the following on the Cisco3 router.
+
+ip name-server 1.1.1.1
+ip name-server 1.0.0.1
+ip domain-lookup
+
+Experiment with fast_cli=True to see how long the script takes to execute (with and without this option enabled).
+
+Verify DNS lookups on the router are now working by executing 'ping google.com'. Verify from this that you receive a ping response back.'''
+
 from netmiko import ConnectHandler
 from getpass import getpass
 from datetime import datetime
@@ -31,4 +41,3 @@ print(op2)
 print(f"Total time taken with fast_cli {end_time2 - st_time2}")
 
 xe_connect2.disconnect()
-
