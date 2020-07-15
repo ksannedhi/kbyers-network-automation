81423ac75729b814e8bd27bf654dcf56d77f66aa
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 15:07:05 +0530

    Update Ex7.py

--- Class7/Ex7.py
+++ Class7/Ex7.py
@@ -1,3 +1,13 @@
+'''7a. Create an nxapi_plumbing "Device" object for nxos1. The api_format should be "xml" and the transport should be "https" (port 8443).
+Use getpass() to capture the device's password. Send the "show interface Ethernet1/1" command to the device, parse the output, and print out the following information:
+Interface: Ethernet1/1; State: up; MTU: 1500
+
+7b. Run the following two show commands on the nxos1 device using a single method and passing in a list of commands: "show system uptime" and "show system resources".
+Print the XML output from these two commands.
+
+7c. Using the nxapi_plumbing config_list() method, configure two loopbacks on nxos1 including interface descriptions.
+Pick random loopback interface numbers between 100 and 199.'''
+
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from getpass import getpass
@@ -33,4 +43,3 @@ conf_cmds = [
 conf_output = device.config_list(conf_cmds)
 for cmd in conf_output:
    print(etree.tostring(cmd, encoding = "unicode"))
-
