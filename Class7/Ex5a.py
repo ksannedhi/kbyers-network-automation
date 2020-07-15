52ce66b915fdeb8b437fed9eaf82f6a682aabaf0
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 14:58:37 +0530

    Update Ex5a.py

--- Class7/Ex5a.py
+++ Class7/Ex5a.py
@@ -1,3 +1,8 @@
+'''Load the show_version.xml file (originally from a Cisco NX-OS device) using the etree.fromstring() method.
+Note this XML document, unlike the previous documents, contains the document encoding information.
+Because the document encoding is at the top of the file, you will need to read the file using "rb" mode (the "b" signifies binary mode).
+Print out the the namespace map of this XML object. You can accomplish this by using the .nsmap attribute of your XML object.'''
+
 from lxml import etree
 
 xml_file = open("Ex5_show_version.xml", "rb")
