e235cee869cc570eb169555075e68aacde29174c
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 14:45:59 +0530

    Update Ex2b.py

--- Class7/Ex2b.py
+++ Class7/Ex2b.py
@@ -1,3 +1,8 @@
+'''Print the names and an index number of each security zone in the XML data from Exercise 2a. Your output should look similar to the following (tip, enumerate will probably help):
+Security Zone #1: trust
+Security Zone #2: untrust
+Security Zone #3: junos-host'''
+
 import xmltodict
 
 xml_file = open("Ex1_show_security_zones.xml")
@@ -8,4 +13,3 @@ zones_list = my_dict["zones-information"]["zones-security"]
 
 for index, zone in enumerate(zones_list, 1):
     print(f'Security Zone #{index}: {zone["zones-security-zonename"]}')
-
