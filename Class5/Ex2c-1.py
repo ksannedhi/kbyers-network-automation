0a0ee258125a45c1d882ff0f5315d1b7ccfd0444
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 13:46:06 +0530

    Update Ex2c-1.py

--- Class5/Ex2c-1.py
+++ Class5/Ex2c-1.py
@@ -1,3 +1,11 @@
+'''Use Netmiko to push the configurations generated in exercise 2b to the nxos1 device and to the nxos2 device, respectively.
+Verify you are able to ping between the devices and also verify that the BGP session reaches the established state.
+Note, you might need to use an alternate interface besides Ethernet 1/1 (you can use either Ethernet 1/1, 1/2, 1/3, or 1/4).
+Additionally, you might need to use a different IP network (to avoid conflicts with other students). Your autonomous system should remain 22, however.
+
+For this exercise you should store your Netmiko connection dictionaries in an external file named my_devices.py and should import nxos1, and nxos2 from that external file.
+Make sure that you use getpass() to enter the password in for these devices (as opposed to storing the definitions in the file).'''
+
 from my_devices import nxos1_dict, nxos2_dict
 from netmiko import ConnectHandler
 from jinja2 import FileSystemLoader, StrictUndefined
@@ -26,4 +34,3 @@ for device_dict in (nxos1_dict, nxos2_dict):
     nxos_cmd_op = nxos_connect.send_config_set(j2_op_lines)
     nxos_connect.disconnect()
     print(nxos_cmd_op)
-
