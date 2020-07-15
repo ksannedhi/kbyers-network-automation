4c8e8827357d499af168faea14af456b8d6b425a
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 13:57:28 +0530

    Update Ex2a.py

--- Class6/Ex2a.py
+++ Class6/Ex2a.py
@@ -1,3 +1,10 @@
+'''Define an Arista device in an external YAML file (use arista4.lasthop.io for the device).
+In your YAML file, make sure the key names exactly match the names required for use with pyeapi and the connect() method.
+In other words, you should be able to execute 'connect(**device_dict)' where device_dict was retrieved from your YAML file.
+Do not store the lab password in this YAML file, instead set the password using getpass() in your Python program.
+Using this Arista device information stored in a YAML file, repeat the 'show ip arp' retrieval using pyeapi.
+Once again, from this ARP table data, print out a mapping of all of the IP addresses and their corresponding MAC addresses.'''
+
 import yaml
 from getpass import getpass
 import pyeapi
@@ -16,4 +23,3 @@ ip_mac_list = output[0]["result"]["ipV4Neighbors"]
 
 for arp_entry in ip_mac_list:
     print(f'{arp_entry["address"]} --> {arp_entry["hwAddress"]}')
-
