13458771272cc70f547d954c2ff5692ce7e7fe0f
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 14:54:15 +0530

    Update Ex4b.py

--- Class7/Ex4b.py
+++ Class7/Ex4b.py
@@ -1,3 +1,5 @@
+'''Use the find() method to find the first "zones-security-zonename". Print out the zone name for that element (the "text" of that element).'''
+
 from lxml import etree
 
 xml_file = open("Ex1_show_security_zones.xml")
@@ -6,4 +8,3 @@ my_xml = etree.fromstring(xml_data)
 
 first_sec_zonename = my_xml.find("./zones-security/zones-security-zonename")
 print(first_sec_zonename.text)
-
