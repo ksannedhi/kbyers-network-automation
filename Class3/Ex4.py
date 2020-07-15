29b4893dd7bb69a362af57819d2a885bd2a79f3c
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 06:58:47 +0530

    Update Ex4.py

--- Class3/Ex4.py
+++ Class3/Ex4.py
@@ -1,3 +1,30 @@
+'''You have the following JSON ARP data from an Arista switch:
+
+{
+    "dynamicEntries": 2,
+    "ipV4Neighbors": [
+        {
+            "hwAddress": "dc38.e111.97cf",
+            "address": "172.17.17.1",
+            "interface": "Ethernet45",
+            "age": 0
+        },
+        {
+            "hwAddress": "90e2.ba5c.25fd",
+            "address": "172.17.16.1",
+            "interface": "Ethernet36",
+            "age": 0
+        }
+    ],
+    "notLearnedEntries": 0,
+    "totalEntries": 2,
+    "staticEntries": 0
+} <-- This is written to Ex4.json
+
+From a file, read this JSON data into your Python program.
+Process this ARP data and return a dictionary where the dictionary keys are the IP addresses and the dictionary values are the MAC addresses.
+Print this dictionary to standard output.'''
+
 import json
 
 with open("Ex4.json") as f:
