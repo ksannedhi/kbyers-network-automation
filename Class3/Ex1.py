20cff7db96603dd6dffbc9ad4116afffbf31d379
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 06:47:46 +0530

    Update Ex1.py

--- Class3/Ex1.py
+++ Class3/Ex1.py
@@ -1,3 +1,12 @@
+'''Using the below ARP data, create a five element list. Each list element should be a dictionary with the following keys: "mac_addr", "ip_addr", "interface".
+At the end of this process, you should have five dictionaries contained inside a single list.
+Protocol  Address      Age  Hardware Addr   Type  Interface
+Internet  10.220.88.1   67  0062.ec29.70fe  ARPA  Gi0/0/0
+Internet  10.220.88.20  29  c89c.1dea.0eb6  ARPA  Gi0/0/0
+Internet  10.220.88.22   -  a093.5141.b780  ARPA  Gi0/0/0
+Internet  10.220.88.37 104  0001.00ff.0001  ARPA  Gi0/0/0
+Internet  10.220.88.38 161  0002.00ff.0001  ARPA  Gi0/0/0'''
+
 from pprint import pprint
 
 arp_data = """
