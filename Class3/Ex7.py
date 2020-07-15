1a2af83759ce24ba4357ea2ac3b965910c772cc4
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 07:05:23 +0530

    Update Ex7.py

--- Class3/Ex7.py
+++ Class3/Ex7.py
@@ -1,3 +1,29 @@
+'''You have the following BGP configuration from a Cisco IOS-XR router:
+router bgp 44
+ bgp router-id 10.220.88.38
+ address-family ipv4 unicast
+ !
+ neighbor 10.220.88.20
+  remote-as 42
+  description pynet-rtr1
+  address-family ipv4 unicast
+   route-policy ALLOW in
+   route-policy ALLOW out
+  !
+ !
+ neighbor 10.220.88.32
+  remote-as 43
+  address-family ipv4 unicast
+   route-policy ALLOW in
+   route-policy ALLOW out
+
+From this BGP configuration, retrieve all of BGP peer IP addresses and their corresponding remote-as. Return a list of tuples. 
+The tuples should be (neighbor_ip, remote_as). Print your data-structure to standard output.
+
+Your output should look similar to the following. Use ciscoconfparse to accomplish this.
+​BGP Peers: 
+[('10.220.88.20', '42'), ('10.220.88.32', '43')]'''
+
 from ciscoconfparse import CiscoConfParse
 
 bgp_config = """
