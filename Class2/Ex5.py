bacb966f5c385fe9eb900baa500c0cf8502f2210
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 06:12:26 +0530

    Update Ex5.py

--- Class2/Ex5.py
+++ Class2/Ex5.py
@@ -1,3 +1,7 @@
+'''On both the NXOS1 and NXOS2 switches configure five VLANs including VLAN names (just pick 5 VLAN numbers between 100 - 999).
+Use Netmiko's send_config_from_file() method to accomplish this.
+Also use Netmiko's save_config() method to save the changes to the startup-config.'''
+
 from netmiko import ConnectHandler
 from getpass import getpass
 password = getpass()
