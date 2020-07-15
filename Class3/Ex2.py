3a680d75cb7007841f01ccd8e34e054105fd651d
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 06:49:03 +0530

    Update Ex2.py

--- Class3/Ex2.py
+++ Class3/Ex2.py
@@ -1,3 +1,7 @@
+'''Create a list where each of the list elements is a dictionary representing one of the network devices in the lab. Do this for at least four of the lab devices.
+The dictionary should have keys corresponding to the device_name, host (i.e. FQDN), username, and password.
+Use a fictional username/password to avoid checking the lab password into GitHub.'''
+
 import yaml
 from pprint import pprint
 
