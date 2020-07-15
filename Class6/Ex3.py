e5b5759c6e8ac6c1cdafc7060017109ee24da602
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 14:30:32 +0530

    Update Ex3.py

--- Class6/Ex3.py
+++ Class6/Ex3.py
@@ -1,3 +1,8 @@
+'''Using your external YAML file and your function located in my_funcs.py, use pyeapi to connect to arista4.lasthop.io and retrieve "show ip route".
+From this routing table data, extract all of the static and connected routes from the default VRF.
+Print these routes to the screen and indicate whether the route is a connected route or a static route.
+In the case of a static route, print the next hop address.'''
+
 from Ex2b_my_funcs import yaml_load
 from getpass import getpass
 import pyeapi
