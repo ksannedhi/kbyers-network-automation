e1a20f133aaf5b4a07f3591fd492ad7e0723f1b0
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 13:42:24 +0530

    Update Ex2a.py

--- Class5/Ex2a.py
+++ Class5/Ex2a.py
@@ -1,3 +1,14 @@
+'''Use Python and Jinja2 to generate the below NX-OS interface configuration. You should use an external template file and a Jinja2 environment to accomplish this.
+The interface, ip_address, and netmask should all be variables in the Jinja2 template.
+ 
+nxos1
+interface Ethernet1/1
+  ip address 10.1.100.1/24
+
+nxos2
+interface Ethernet1/1
+  ip address 10.1.100.2/24'''
+
 from jinja2 import FileSystemLoader, StrictUndefined
 from jinja2.environment import Environment
 
