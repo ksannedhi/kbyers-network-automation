129bc93a75d913534deae0a69fb1558a1840aed0
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 13:48:24 +0530

    Update Ex3.py

--- Class5/Ex3.py
+++ Class5/Ex3.py
@@ -1,3 +1,23 @@
+'''Generate the following configuration output from an external Jinja2 template:
+vrf definition blue
+ rd 100:1
+ !
+ address-family ipv4
+  route-target export 100:1
+  route-target import 100:1
+ exit-address-family
+ !
+ address-family ipv6
+  route-target export 100:1
+  route-target import 100:1
+ exit-address-family
+
+Both the IPv4 and the IPv6 address families should be controlled by Jinja2 conditionals (in other words, the entire 'address-family ipv4' section
+and the entire 'address-family ipv6' sections can be dropped from the generated output depending on the value of two variables
+that you pass into your template--for example, the 'ipv4_enabled' and the 'ipv6_enabled' variables).
+Additionally, both the vrf_name and the rd_number should be variables in the template.
+Make sure that you control the whitespace in your output such that the configuration looks visually correct.'''
+
 from jinja2 import FileSystemLoader, StrictUndefined
 from jinja2.environment import Environment
 
@@ -8,4 +28,3 @@ vrf_vars = {"vrf_name": "blue", "rd_number": "100:1", "ipv4_enabled": True, "ipv
 
 t = env.get_template("Ex3.j2")
 print(t.render(vrf_vars))
-
