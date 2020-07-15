a681033b05c5f1b9510c958f676cfef4c5813789
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-15 13:59:57 +0530

    Update Ex2b.py

--- Class6/Ex2b.py
+++ Class6/Ex2b.py
@@ -1,3 +1,7 @@
+'''Create a Python module named 'Ex2b_my_funcs.py'. In this file create two functions: function1 should read the YAML file you created in exercise 2a and return the corresponding data structure;
+function2 should handle the output printing of the ARP entries (in other words, create a separate function that handles all printing to standard out of the 'show ip arp' data).
+Create a new Python program based on exercise2a except the YAML file loading and the output printing is accomplished using the functions defined in my_funcs.py.'''
+
 from Ex2b_my_funcs import yaml_load, print_arp_entries
 from getpass import getpass
 import pyeapi
