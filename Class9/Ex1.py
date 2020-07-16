1d3fe96fc2532c2ecb91a5e48ee2eb0f9bc81bb8
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 12:19:54 +0530

    Update Ex1.py

--- Class9/Ex1.py
+++ Class9/Ex1.py
@@ -1,3 +1,14 @@
+'''1a. Create a Python file named "my_devices.py" that defines the NAPALM connection information for both the 'cisco3' device and the 'arista1' device.
+Use getpass() for the password handling. This Python module should be used to store the device connection information for all of the exercises in this lesson.
+
+1b. Create a simple function that accepts the NAPALM device information from the my_devices.py file and creates a NAPALM connection object.
+This function should open the NAPALM connection to the device and should return the NAPALM connection object.
+
+1c. Using your "my_devices.py" file and your NAPALM connection function, create a list of NAPALM connection objects to 'cisco3' and 'arista1'.
+
+1d. Iterate through the connection objects, print out the device's connection object itself.
+Additionally, pretty print the facts for each device and also print out the device's NAPALM platform type (ios, eos, et cetera).'''
+
 from napalm import get_network_driver
 from Ex1_devices import my_devices
 from pprint import pprint
@@ -15,4 +26,3 @@ for dev_dict in my_devices:
     print("=" * 79)
     pprint(device.get_facts())
     print("=" * 79)
-
