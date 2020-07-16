62367b6443e731b3a256fda229e1b0ea6691fe23
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 10:41:58 +0530

    Update Ex3a.py

--- Class8/Ex3a.py
+++ Class8/Ex3a.py
@@ -1,3 +1,25 @@
+"""3a. Open a connection to the srx2 device and acquire a configuration lock.
+Validate that the configuration session is indeed locked by SSH'ing into the device and attempting to enter configuration mode ("configure").
+Reuse, the 'srx2' device definition from the jnpr_devices.py file that you created in exercise2.
+
+You should receive a prompt similar to the following:
+pyclass@srx2> configure
+Entering configuration mode
+Users currently editing the configuration:
+  pyclass (pid 30316) on since 2019-03-08 18:30:51 PST
+      exclusive
+
+Add code to attempt to lock the configuration again. Gracefully handle the "LockError" exception (meaning the configuration is already locked).
+
+3b. Use the "load" method to stage a configuration using a basic set command, for example, "set system host-name python4life".
+
+3c. Print the diff of the current configuration with the staged configuration. Your output should look similar to the following:
+[edit system]
+-  host-name srx2;
++  host-name python4life;
+
+3d. Rollback the staged configuration. Once again, print out the diff of the staged and the current configuration (which at this point should be None)."""
+
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
 from jnpr.junos.exception import LockError
@@ -20,4 +42,3 @@ print(cfg.diff())
 
 cfg.rollback(0)
 print(cfg.diff())
-
