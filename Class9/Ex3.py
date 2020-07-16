80ef54a0ee94fc07e2475efcd6826cfb9d0ddbfb
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 12:23:42 +0530

    Update Ex3.py

--- Class9/Ex3.py
+++ Class9/Ex3.py
@@ -1,3 +1,19 @@
+'''3a. Using your existing functions and the my_devices.py file, create a NAPALM connection to both cisco3 and arista1.
+
+3b. Create two new text files `arista1.lasthop.io-loopbacks` and `cisco3.lasthop.io-loopbacks`. In each of these files, create two new loopback interfaces with a description.
+Your files should be similar to the following:
+interface loopback100
+  description loopback100
+!
+interface loopback101
+  description loopback101
+
+For both cisco3 and arista1, use the load_merge_candidate() method to stage the candidate configuration.
+In other words, use load_merge_candidate() and your loopback configuration file to stage a configuration change.
+Use the NAPALM compare_config() method to print out the pending differences (i.e. the differences between the running configuration and the candidate configuration).
+
+3c. Commit the pending changes to each device, and check the diff once again (after the commit_config).'''
+
 from Ex2_functions import open_napalm_connection
 from Ex1_devices import my_devices
 
@@ -18,4 +34,3 @@ for dev_dict in my_devices:
 
     device.commit_config()
     print("Following changes are committed successfully.")
-
