68f1cac52c28fee2cf31f169dd19f68bd949ef28
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 12:11:26 +0530

    Delete basic_conn.py

--- Class8/basic_conn.py
+++
@@ -1,7 +0,0 @@
-from jnpr.junos import Device
-from getpass import getpass
-from pprint import pprint
-
-srx1 = (Device(host = "srx1.lasthop.io", user = "pyclass", password = getpass())).open()
-pprint(srx1.facts)
-
