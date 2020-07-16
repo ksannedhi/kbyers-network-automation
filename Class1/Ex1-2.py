d793080b1c0fc07a43c8ed6d4357cce76ec0de1b
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 09:19:52 +0530

    Update Ex1-2.py

--- Class1/Ex1-2.py
+++ Class1/Ex1-2.py
@@ -1,3 +1,7 @@
+'''Add a second NX-OS device to your first exercise. Make sure you are using dictionaries to represent the two NX-OS devices.
+Additionally, use a for-loop to accomplish the Netmiko connection creation
+Once again print the prompt back from the devices that you connected to.'''
+
 from netmiko import ConnectHandler
 from getpass import getpass
 
@@ -9,4 +13,3 @@ for nxos_router in (nxos1_dict, nxos2_dict):
     nxos_connect = ConnectHandler(**nxos_router)
     print(nxos_connect.find_prompt())
     nxos_connect.disconnect()
-
