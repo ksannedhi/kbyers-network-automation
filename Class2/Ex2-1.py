dcb96eda9920ea50c31f096ad8604c5705bae29d
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 09:23:42 +0530

    Update Ex2-1.py

--- Class2/Ex2-1.py
+++ Class2/Ex2-1.py
@@ -1,3 +1,9 @@
+'''Create a Netmiko connection to the 'nxos2' device using a global_delay_factor of 2.
+Execute 'show lldp neighbors detail' and print the returned output to standard output.
+Execute 'show lldp neighbors detail' a second time using send_command() with a delay_factor of 8. Print the output of this command to standard output.
+Use the Python datetime library to record the execution time of both of these commands.
+Print these execution times to standard output.'''
+
 from netmiko import ConnectHandler
 from getpass import getpass
 from datetime import datetime
@@ -18,4 +24,3 @@ output2 = nxos2_connect.send_command("sh lldp nei det", delay_factor = 8)
 print(output2)
 time2_after = datetime.now()
 print(f"Time taken for command execution with the local delay factor: {time2_after - time2_before}")
-
