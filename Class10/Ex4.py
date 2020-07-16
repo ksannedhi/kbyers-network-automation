6ac6ad28a0f1fc333c67c3f062135ef3e949e109
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 12:37:20 +0530

    Update Ex4.py

--- Class10/Ex4.py
+++ Class10/Ex4.py
@@ -1,3 +1,5 @@
+'''Create a new program that completes the same task as Exercise 3b except using multiple processes (i.e. a 'ProcessPoolExecutor').'''
+
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from Ex1_devices import device_list
 from Ex2_functions import ssh_command2
@@ -18,4 +20,3 @@ for future in as_completed(futures):
 end_time = datetime.now()
 
 print(f"Total time taken to execute 'show version' on all the devices: {end_time - start_time}")
-
