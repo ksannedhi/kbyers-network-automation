23578a959e31d7cd4f13b8180101dec29576164b
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 10:37:34 +0530

    Update Ex2b.py

--- Class8/Ex2b.py
+++ Class8/Ex2b.py
@@ -1,3 +1,10 @@
+'''2a. Create a Python module named jnpr_devices.py. This Python module should contain a dictionary named "srx2".
+This "srx2" dictionary should contain all of the key-value pairs needed to establish a PyEZ connection. You should use getpass() for the password handling.
+You should import this "srx2" device definition for all of the remaining exercises in class8.
+
+2b. Create a Python program that creates a PyEZ Device connection to "srx2" (using the previously created Python module).
+Using this PyEZ connection and the RouteTable and ArpTable views retrieve the routing table and the arp table for srx2.'''
+
 from jnpr_devices import srx1
 from jnpr.junos import Device
 from jnpr.junos.op.routes import RouteTable
