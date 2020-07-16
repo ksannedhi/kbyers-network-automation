25ccc6459750a3c19ddc5d21dd57c8004e072885
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 10:39:13 +0530

    Update Ex2b.py

--- Class8/Ex2b.py
+++ Class8/Ex2b.py
@@ -3,7 +3,16 @@ This "srx2" dictionary should contain all of the key-value pairs needed to estab
 You should import this "srx2" device definition for all of the remaining exercises in class8.
 
 2b. Create a Python program that creates a PyEZ Device connection to "srx2" (using the previously created Python module).
-Using this PyEZ connection and the RouteTable and ArpTable views retrieve the routing table and the arp table for srx2.'''
+Using this PyEZ connection and the RouteTable and ArpTable views retrieve the routing table and the arp table for srx2.
+
+This program should have four separate functions:
+1. check_connected() - Verify that your NETCONF connection is working. You can use the .connected attribute to check the status of this connection.
+2. gather_routes() - Return the routing table from the device.
+3. gather_arp_table() - Return the ARP table from the device.
+4. print_output() - A function that takes the Juniper PyEZ Device object, the routing table, and the ARP table and then prints out the:
+hostname, NETCONF port, username, routing table, ARP table
+
+This program should be structured such that all of the four functions could be reused in other class8 exercises.'''
 
 from jnpr_devices import srx1
 from jnpr.junos import Device
@@ -42,4 +51,3 @@ def print_output():
     print()
 
 print_output()
-
