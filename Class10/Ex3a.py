57bdb195a49dd58b20c37f61c484a194a4137e22
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 12:35:08 +0530

    Update Ex3a.py

--- Class10/Ex3a.py
+++ Class10/Ex3a.py
@@ -1,3 +1,10 @@
+'''Create a new function that is a duplicate of your "ssh_command" function. Name this function "ssh_command2".
+This function should eliminate all printing to standard output and should instead return the show command output.
+Note, in general, it is problematic to print in the child thread as you can get into race conditions between the threads.
+Using the "ThreadPoolExecutor" in Concurrent Futures execute "show version" on each of the devices defined in my_devices.py.
+Use the 'wait' method to ensure all of the futures have completed. Concurrent futures should be executing the ssh_command2 function in the child threads.
+Print the total execution time required to accomplish this task.'''
+
 from concurrent.futures import ThreadPoolExecutor, wait
 from Ex2_functions import ssh_command2
 from Ex1_devices import device_list
@@ -20,4 +27,3 @@ for future in futures:
 end_time = datetime.now()
 
 print(f"Total time taken to execute 'show version' on all the devices: {end_time - start_time}")
-
