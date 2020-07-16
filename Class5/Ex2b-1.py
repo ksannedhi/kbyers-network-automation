3d3db5683c2ff43187a405d2a8af7fe5d59ce201
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 10:23:48 +0530

    Update Ex2b-1.py

--- Class5/Ex2b-1.py
+++ Class5/Ex2b-1.py
@@ -1,3 +1,19 @@
+'''Expand your Jinja2 template such that both the following interface and BGP configurations are generated for nxos1 and nxos2.
+The interface name, IP address, netmask, local_as, and peer_ip should all be variables in the template.
+This is iBGP so the remote_as will be the same as the local_as.
+nxos1
+interface Ethernet1/1
+  ip address 10.1.100.1/24
+router bgp 22
+  neighbor 10.1.100.2 remote-as 22
+    address-family ipv4 unicast
+nxos2
+interface Ethernet1/1
+  ip address 10.1.100.2/24
+router bgp 22
+  neighbor 10.1.100.1 remote-as 22
+    address-family ipv4 unicast'''
+
 from jinja2 import FileSystemLoader, StrictUndefined
 from jinja2.environment import Environment
 
@@ -14,4 +30,3 @@ nxos2_vars["neighbor_ip"] = nxos1_vars["ip_addr"]
 for nxos_vars in (nxos1_vars, nxos2_vars):
     t = env.get_template("Ex2b-1.j2")
     print(t.render(nxos_vars))
-
