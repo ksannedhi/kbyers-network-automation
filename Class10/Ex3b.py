cdb522e15f5e7587445dd0c2ca39352d173fa453
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 12:36:12 +0530

    Update Ex3b.py

--- Class10/Ex3b.py
+++ Class10/Ex3b.py
@@ -1,3 +1,7 @@
+'''Instead of waiting for all of the futures to complete, use "as_completed" to print the future results as they come available.
+Reuse your "ssh_command2" function to accomplish this. Once again use the concurrent futures "ThreadPoolExecutor" and print the "show version" results to standard output.
+Additionally, print the total execution time to standard output.'''
+
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from Ex1_devices import device_list
 from Ex2_functions import ssh_command2
@@ -18,4 +22,3 @@ for future in as_completed(futures):
 end_time = datetime.now()
 
 print(f"Total time taken to execute 'show version' on all the devices: {end_time - start_time}")
-
