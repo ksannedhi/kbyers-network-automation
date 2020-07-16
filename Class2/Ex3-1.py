e68aa6c6f4dbe51e9394ca42fbbef72ebcba136c
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 09:25:01 +0530

    Update Ex3-1.py

--- Class2/Ex3-1.py
+++ Class2/Ex3-1.py
@@ -1,3 +1,11 @@
+'''On your AWS lab server, look at the ntc-templates index file (at ~/ntc-templates/templates/index).
+Look at some of the commands available for cisco_ios (you can use 'cat ~/ntc-templates/templates/index | grep cisco_ios' to see this).
+Also look at some of the abbreviated forms of Cisco IOS commands that are supported in the index file.
+Create a script using Netmiko that executes 'show version' and 'show lldp neighbors' against the Cisco4 device with use_textfsm=True.
+What is the outermost data structure that is returned from 'show lldp neighbors' (dictionary, list, string, something else)? 
+The Cisco4 device should only have one LLDP entry (the HPE switch that this router connects to). From this LLDP data, print out the remote device's interface.
+In other words, print out the port number on the HPE switch that Cisco4 connects into.'''
+
 from netmiko import ConnectHandler
 from getpass import getpass
 from pprint import pprint
@@ -15,4 +23,3 @@ for dev_cmd in ("sh version", "sh lldp neighbors"):
         print(f'Remote interface is {output[0]["neighbor_interface"]}')
 
 cisco4_connect.disconnect()
-
