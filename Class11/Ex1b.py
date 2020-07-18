d083502fb89211604219a0cde395541c8024564c
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-18 13:19:33 +0530

    Update Ex1b.py

--- Class11/Ex1b.py
+++ Class11/Ex1b.py
@@ -1,3 +1,7 @@
+'''Use curl to access "https://netbox.lasthop.io/api/dcim/devices/". This API endpoint requires authentication; use the "-H" flag to provide this authentication information. Note, the lab has a NETBOX_TOKEN environment variable. Consequently, you should be able to do the following:
+
+curl -H "Authorization: Token $NETBOX_TOKEN" https://netbox.lasthop.io/api/dcim/devices/ --insecure | jq'''
+
 import os
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -11,4 +15,3 @@ response = requests.get(url, headers = http_headers, verify = False)
 response = response.json()
 
 pprint(response) #Alternately, use the 'curl -H "Authorization: Token $NETBOX_TOKEN" https://netbox.lasthop.io/api/dcim/devices/ --insecure | jq' command
-
