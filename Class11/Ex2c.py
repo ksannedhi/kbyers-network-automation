89b45ce6d804a7224ac021ddc38f35313765d02f
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-18 13:28:13 +0530

    Update Ex2c.py

--- Class11/Ex2c.py
+++ Class11/Ex2c.py
@@ -1,3 +1,6 @@
+'''Execute another HTTP GET request to retrieve all of the endpoints under the "/api/dcim" parent. Pretty print out the response.json() from this output.
+This should be a dictionary with the key being the next part of the URL after "/api/dcim" and the value being the full URL.'''
+
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -9,4 +12,3 @@ response = requests.get(url, headers = http_headers, verify = False)
 response = response.json()
 
 pprint(response)
-
