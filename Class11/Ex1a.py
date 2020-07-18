219a7f6494fa672c9ebc3e5d1b26e2b3d714e7ef
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-18 13:16:40 +0530

    Update Ex1a.py

--- Class11/Ex1a.py
+++ Class11/Ex1a.py
@@ -1,3 +1,10 @@
+'''Use curl with the "--insecure" option to view the NetBox top-level /api endpoint. An example of this would be:
+curl -L -s https://netbox.lasthop.io/api/ --insecure
+You can also pipe this into the "jq" utility for prettier output:
+
+curl -L -s https://netbox.lasthop.io/api/ --insecure | jq
+Note, you will possibly need to add the "-L" argument to all of the "curl" requests (this instructs "curl" to follow any redirects).'''
+
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -9,4 +16,3 @@ response = requests.get(url, headers = http_headers, verify = False)
 response = response.json()
 
 pprint(response) #Alternately, use the 'curl -L -s https://netbox.lasthop.io/api --insecure | jq' command.
-
